61460801fbed0d1bbaa98200116ca1babf93fdd9
Kiran Surdhar <ksurdhar1@gmail.com>
2019-04-14 11:22:57 -0600

    repositions on key nav and pointer clicks:

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ document.onreadystatechange = function() {
     var CoordinateHelper = new CaretCoordinates(textArea);
 
     function onInputHandler(evt) {
+      console.log('on input')
 			var newRows = Math.ceil((this.scrollHeight - baseScrollHeight) / 14);
 			this.rows = 1 + newRows;
-      this.style.height = this.rows*14 + 'px';
+      this.style.height = this.rows*14 + 'px'; // this changes the height based upon our rows
       recenter(this);
     }
 
@@ -27,14 +28,32 @@ document.onreadystatechange = function() {
 
       // this way we have a scroll bar and ought to be able to scroll up to the top if necessary.
       // when typing 
-
+      console.log('recentering:')
       console.log('--------------------------------------');
+      console.log('SELECTION START', textarea.selectionStart);
+      console.log('SELECTION END', textarea.selectionEnd);
       // console.log('COORDINATES: ', coordinates);
       // console.log('TOTAL ROWS:', textarea.rows);
       // console.log('CURRENT ROW: ', currentRow);
       // console.log('HEIGHT: ', textarea.style.height);
     }
+
+    function onPointerDown(evt) {
+      setTimeout(recenter.bind(this, this), 10)
+    }
+
+    function onKeyDown(evt) {
+      console.log('keydown!')
+      // setTimeout(() => {
+      //   console.log('firing nowww')
+      //   recenter(this)
+      // }, 10)
+      setTimeout(recenter.bind(this, this), 10)
+    }
     textArea.addEventListener("input", onInputHandler);
+    textArea.addEventListener('pointerdown', onPointerDown)
+    textArea.addEventListener('keydown', onKeyDown)
+    
 
     // attempt to make top scrollable
     // var topString = textarea.style.top;
